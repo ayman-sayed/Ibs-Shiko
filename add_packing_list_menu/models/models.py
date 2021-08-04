aba55a97545cce03234b8e1385b4a6703a98bdcc
ayman <aymanshahean@gmail.com>
2021-08-04 12:46:30 +0200

    /

--- add_packing_list_menu/models/models.py
+++ add_packing_list_menu/models/models.py
@@ -25,25 +25,25 @@ class add_packing_list_menu(models.Model):
 
     def git_create_po(self):
         for rec in self:
-            order_linee = []
+            print('rec.vendor_name.id', rec.vendor_name.id)
+            print('rec.vendor_name.name', rec.vendor_name.name)
+            po_create = self.env['purchase.order'].sudo().create({
+                'partner_id': rec.vendor_name.id,
+            })
+
             for line in rec.packing_list:
-                order_linee.append((0, 0, {
+                order_linee = self.env['purchase.order.line'].sudo().create({
                     'product_id': line.product_id_id.id,
                     'name': line.product_description,
                     'product_qty': line.quantity,
                     'product_uom': line.product_id_id.uom_id.id,
-                    'price_unit': 50,
-                    'purchase_order_line':[ line.id]
-                }))
-            print('order_linee',order_linee)
-            po_create=self.env['purchase.order'].sudo().create({
-                    'partner_id': rec.vendor_name.id,
-                    'order_line': order_linee,
+                    'price_unit': 0,
+                    'order_id': po_create.id
                 })
             rec.state = "create_po"
             return {
                 'name': 'Purchase',
-                'domain': [('id', '=', po_create.id)],
+                'res_id': po_create.id,
                 'res_model': 'purchase.order',
                 'type': 'ir.actions.act_window',
                 'view_mode': 'form',
@@ -53,49 +53,9 @@ class add_packing_list_menu(models.Model):
 
 
 
-    # def tap_for_quotation(self):
-    #     order_linee=[]
-    #     attache=[]
-    #     for rec in self:
-    #         for attach in rec.attachment_ids:
-    #             attache.append(attach.id)
-    #         # rec.state="fully_quotationed"
-    #         for line in rec.request_line_ids:
-    #             if line.state != "fully_quotationed":
-    #                 order_linee.append((0,0,{
-    #             'product_id': line.product_id.id,
-    #             'name': line.name,
-    #             'product_qty': line.product_qty,
-    #             'product_uom': line.product_uom_id.id,
-    #             'attachmentt_ids': [a.id for a in line.attachment_ids],
-    #             'purchase_requests_id': self.id,
-    #             'purchase_request_line':[ line.id],
-    #
-    #         }))
-    #                 print("order",order_linee)
-    #
-    #     return {
-    #         'name': 'New Quotation',
-    #         'domain': [],
-    #         'res_model': 'purchase.order',
-    #         'type': 'ir.actions.act_window',
-    #         'view_mode': 'form',
-    #         'view_type': 'form',
-    #         'context': {
-    #             "default_order_line" : order_linee,
-    #             "default_date_planned" : self.due_date,
-    #             "default_request_name" : self.nname,
-    #             "default_purchase_requests" : self.id,
-    #             "default_product_category_id" : self.request_category_id.id,
-    #             "default_attachmentt_ids" : [i.id for i in self.attachment_ids],
-    #             "default_purchase_request_ids" : [self.id],
-    #
-    #                     },
-    #         'target': 'new',
-    #     }
 
 class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
+    _inherit = 'purchase.order.line'
 
     _sql_constraints = [
         ('accountable_required_fields',
@@ -114,5 +74,5 @@ class LineTap(models.Model):
 
     product_id_id = fields.Many2one(comodel_name="product.product", string="Product", )
     picking_id = fields.Many2one(comodel_name="add_packing_list_menu.add_packing_list_menu", tracking=True)
-    product_description = fields.Char(string="Product Description",related='product_id_id.name', tracking=True)
+    product_description = fields.Char(string="Product Description", related='product_id_id.name', tracking=True)
     quantity = fields.Float(string="Quantity", tracking=True)
