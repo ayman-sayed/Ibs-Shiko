1ff8c0e87d76f31ba724e6de7fae7508c0043f6b
ayman <aymanshahean@gmail.com>
2021-08-08 17:13:08 +0200

    /

---
+++ edit_packing_list/models/models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models
+
+
+class add_packing_list_menuadd_packing_list_menu(models.Model):
+    _inherit = 'add_packing_list_menu.add_packing_list_menu'
+
+    def git_create_po(self):
+        po_create = super(add_packing_list_menuadd_packing_list_menu, self).git_create_po()
+        for rec in self:
+            po_create['date_planned'] = rec.delivery_date
+        return po_create
