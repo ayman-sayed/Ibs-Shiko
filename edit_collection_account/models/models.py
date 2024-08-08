38d83888d91f93c21e57864786b3e0173f0eb00a
MahmoudZakiH <90396260+MahmoudZakiH@users.noreply.github.com>
2024-08-08 16:59:40 +0300

    edit_collection_account

--- edit_collection_account/models/models.py
+++ edit_collection_account/models/models.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
-
-
-
-
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 
 
 class EditAccountMove(models.Model):
@@ -21,6 +18,30 @@ class EditAccountMove(models.Model):
 #     account_online_link_state = fields.Float(string="", required=False, )
 #     bank_statement_creation_groupby = fields.Float(string="", required=False, )
 
+    def button_draft(self):
+        AccountMoveLine = self.env['account.move.line']
+        excluded_move_ids = []
+
+        if self._context.get('suspense_moves_mode'):
+            excluded_move_ids = AccountMoveLine.search(AccountMoveLine._get_suspense_moves_domain() + [('move_id', 'in', self.ids)]).mapped('move_id').ids
+
+        for move in self:
+            if move in move.line_ids.mapped('full_reconcile_id.exchange_move_id'):
+                raise UserError(_('You cannot reset to draft an exchange difference journal entry.'))
+            if move.tax_cash_basis_rec_id:
+                raise UserError(_('You cannot reset to draft a tax cash basis journal entry.'))
+            # قم بإلغاء الشرط التالي
+            # if move.restrict_mode_hash_table and move.state == 'posted' and move.id not in excluded_move_ids:
+            #    raise UserError(_('You cannot modify a posted entry of this journal because it is in strict mode.'))
+            # نحن نحذف جميع الإدخالات التحليلية لهذا الجورنال
+            move.mapped('line_ids.analytic_line_ids').unlink()
+
+        self.mapped('line_ids').remove_move_reconcile()
+        self.write({'state': 'draft'})
+
+    def server_button_draft(self):
+        for rec in self:
+            rec.write({'state': 'draft'})
 
 class Collection(models.Model):
     _name = 'collection'
