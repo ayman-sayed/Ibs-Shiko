e84c6b8c8584fcd78f0db9001df75652c11f3700
MahmoudZakiH <90396260+MahmoudZakiH@users.noreply.github.com>
2024-09-24 17:40:46 +0000

    hotfixv2

--- stock_card_report/__manifest__.py
+++ stock_card_report/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Card Report",
     "summary": "Add stock card report on Inventory Reporting.",
-    "version": "17.0.1.0.1",
+    "version": "17.0",
     "category": "Warehouse",
     "website": "https://github.com/OCA/stock-logistics-reporting",
     "author": "Ecosoft, Odoo Community Association (OCA)",
